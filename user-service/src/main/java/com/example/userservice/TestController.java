3a68b36c73473675ccb68a59ce80d6ccc17a435c
dhslrl321 <dhslrl321@gmail.com>
2022-04-15 18:10:37 +0900

    commit

--- user-service/src/main/java/com/example/userservice/TestController.java
+++ user-service/src/main/java/com/example/userservice/TestController.java
@@ -1,12 +1,19 @@
 package com.example.userservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    int a= 10;
+
+    @Value("${eureka.client.service-url}")
+    private String data;
+
     @GetMapping("/")
     public String hello() {
-        return "hello from user-service";
+        return data;
     }
 }
--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,13 +2,9 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableFeignClients
+// @EnableFeignClients
 public class UserServiceApplication {
 
 
